6e57483bea78dd0d76859f10c70785d4dacfe95b
Joseph <indiekaj@gmail.com>
2023-11-29 20:13:14 +0300

    ww

--- KISUMU/views.py
+++ KISUMU/views.py
@@ -1,12 +1,40 @@
-
-
 from django.shortcuts import render
+
+
+
 def home(request):
     return render(request,'index.html',  {'bike': 'home'})
 
 
+def product(request):
+    return render(request, 'product.html', {'nav': 'product'})
+
+def about(request):
+    return render(request, 'about.html', {'nav': 'about'})
+
+
+def services(request):
+    return render(request, 'service.html', {'nav': 'service'})
+
+
+def team(request):
+    return render(request, 'team.html', {'nav': 'team'})
+
+
+def contact(request):
+    return render(request, 'contact.html', {'nav': 'contact'})
+
+
 def viewdata(request):
-    return render(request, 'team.html', {'bike': 'team'})
+    return render(request, 'team.html', {'nav': 'team'})
+
+
+def viewdata(request):
+    return render(request, 'team.html', {'nav': 'team'})
+
+
+def viewdata(request):
+    return render(request, 'team.html', {'nav': 'team'})
 
 
 
